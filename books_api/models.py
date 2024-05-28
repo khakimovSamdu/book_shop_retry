c3cc00a5afe714bee69d27f6205f27df90653d80
khakimovSamdu <allamurodxakimov@gmail.com>
2024-05-28 09:24:48 +0500

    auther models

--- books_api/models.py
+++ books_api/models.py
@@ -2,10 +2,23 @@ from django.db import models
 
 # Create your models here.
 class Auther(models.Model):
-    pass
+    author_id = models.IntegerField()
+    firstname = models.CharField(max_length=45)
+    lastname = models.CharField(max_length=45)
+    info = models.TextField()
+    nic_name = models.CharField(max_length=56)
+    def __str__(self) -> str:
+        return self.firstname + " " + self.lastname
 
 class Genre(models.Model):
     pass
 
 class Books(models.Model):
+    order_id = models.IntegerField()
+    title = models.CharField(max_length=64)
+    page = models.IntegerField()
+    quantity = models.IntegerField()
+    price = models.FloatField()
+
+class Publisher(models.Model):
     pass
