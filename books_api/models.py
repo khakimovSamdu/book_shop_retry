4271a696631fe2b7bd7ac0c5488603b03140a446
khakimovSamdu <allamurodxakimov@gmail.com>
2024-06-15 09:07:06 +0500

    genre models atributes

--- books_api/models.py
+++ books_api/models.py
@@ -11,7 +11,11 @@ class Auther(models.Model):
         return self.firstname + " " + self.lastname
 
 class Genre(models.Model):
-    pass
+    genre_id = models.IntegerField()
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
 
 class Books(models.Model):
     pass
